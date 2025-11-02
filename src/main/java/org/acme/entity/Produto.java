f2580ecf019534dfaff8f144f8203e1510ea5dcd
lucasllf2608 <lucas.llf2608@gmail.com>
2025-11-01 23:05:03 -0300

    criando entidade produto

--- src/main/java/org/acme/entity/Produto.java
+++ src/main/java/org/acme/entity/Produto.java
@@ -1,5 +1,25 @@
 package org.acme.entity;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import java.math.BigDecimal; 
 
-public class Produto {
+
+@Entity
+public class Produto extends PanacheEntity {
+
+    public String nome;
+    public String descricao;
+    public BigDecimal preco;
+    public int estoque;
+
     
+    public Produto() {
+    }
+
+    public Produto(String nome, String descricao, BigDecimal preco, int estoque) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
 }
