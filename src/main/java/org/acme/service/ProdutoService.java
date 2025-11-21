91aca4f94d3ea304d36f1431a9d41269b9922bc7
lucasllf2608 <lucas.llf2608@gmail.com>
2025-11-21 00:43:55 -0300

    configurando banco h2

--- src/main/java/org/acme/service/ProdutoService.java
+++ src/main/java/org/acme/service/ProdutoService.java
@@ -1,5 +1,13 @@
 package org.acme.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+
+
+@ApplicationScoped
 public class ProdutoService {
     
 }
